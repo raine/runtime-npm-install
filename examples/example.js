c46e075d669f4f2ddd815f14c061340765582d95
Raine Virta <raine.virta@gmail.com>
2019-01-05 16:58:19 +0200

    add examples

---
+++ examples/example.js
@@ -0,0 +1,20 @@
+const { npmInstallAsync } = require('.')
+const path = require('path')
+const taim = require('taim')
+
+const tmpDir = path.join(require('os').tmpdir(), 'my-app')
+console.log('tmpDir', tmpDir)
+
+;(async () => {
+  await taim('first npmInstallAsync', npmInstallAsync)([
+    'treis@2.6.0',
+    'lodash',
+    'ramda@0.26.1'
+  ], tmpDir)
+
+  await taim('second npmInstallAsync', npmInstallAsync)([
+    'treis@2.6.0',
+    'lodash',
+    'ramda@0.26.1'
+  ], tmpDir)
+})()
